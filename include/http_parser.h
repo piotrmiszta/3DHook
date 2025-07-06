e9c7bf3fa43e8a17a03dde579dcd6911ac4e3dc0
Piotr Miszta <miszta.piotr.pm@gmail.com>
2025-07-06 09:19:21 +0200

    Header for parsing

---
+++ include/http_parser.h
@@ -0,0 +1,40 @@
+#ifndef HTTP_PARSER_H_
+#define HTTP_PARSER_H_
+
+#include "err_codes.h"
+#include "str.h"
+
+enum HttpVersionE
+{
+    HTTP_VERSION_0_9 = 0,
+    HTTP_VERSION_1_0,
+    HTTP_VERSION_1_1,
+    HTTP_VERSION_2,
+    HTTP_VERSION_3,
+};
+
+enum HttpMethodE
+{
+    HTTP_METHOD_GET = 0,
+    HTTP_METHOD_POST,
+};
+
+typedef struct HttpHeaderElement
+{
+    str_view_t key;
+    str_view_t value;
+} HttpHeaderElement;
+
+typedef struct HttpMessage
+{
+    str_t message_buffer;
+    str_view_t url;
+    enum HttpMethodE method;
+    enum HttpVersionE version;
+    u64 elements_count;
+    HttpHeaderElement *elements;
+} HttpMessage;
+
+err_t http_message_parse(HttpMessage message[static 1], str_t buff);
+
+#endif
